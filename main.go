88e3410222a8898ccca35623f1022d6b026078b7
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-06-14 16:56:46 +0100

    updates

--- main.go
+++ main.go
@@ -1,37 +1,18 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-)
-
-type Data struct {
-	Message string `json:"message"`
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	http.HandleFunc("/data", handleData)
-	log.Printf("Server started on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func handleData(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
-	w.Header().Set("Content-Type", "application/json")
-
-	data := &Data{Message: "Hello from Go!"}
-	json.NewEncoder(w).Encode(data)
+	r := gin.Default()
+	r.GET("", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello from PipeOps.io",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
