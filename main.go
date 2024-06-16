0606ef5ee2b3e34ddeb0496b2b29431053b3e648
enyeneraph <enyenerudolf@gmail.com>
2024-06-16 04:10:56 +0100

    Defined structs

--- main.go
+++ main.go
@@ -1,37 +1,17 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-)
-
-type Data struct {
-	Message string `json:"message"`
+type Processor struct {
+    Manufacturer string
+    Brand        string
+    Modifier     string
+    Generation   int
+    Cores        int
+    ClockSpeed   float64
 }
-
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	http.HandleFunc("/data", handleData)
-	log.Printf("Server started on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func handleData(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
-	w.Header().Set("Content-Type", "application/json")
-
-	data := &Data{Message: "Hello from Go!"}
-	json.NewEncoder(w).Encode(data)
+type GPU struct {
+    Form         string
+    Manufacturer string
+    BrandPrefix  string
+    Cores        int
+    VRAM         int
 }
